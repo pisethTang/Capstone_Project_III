967375922a812bfabfd6627e3da944c534a86fc7
Tang Piseth <tangpiseth43@gmail.com>
2025-10-22 11:33:03 +1030

    A: pushes

---
+++ build/CMakeFiles/main-intro.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main-intro.dir/main-intro.cpp.o"
+  "CMakeFiles/main-intro.dir/main-intro.cpp.o.d"
+  "main-intro"
+  "main-intro.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main-intro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/main-intro.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for main-intro.
+# This may be replaced when dependencies are built.
