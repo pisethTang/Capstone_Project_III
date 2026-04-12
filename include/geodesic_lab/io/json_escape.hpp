507292238bbe129107b0bf9360edb87cbc0bbcad
Tang Piseth <tangpiseth43@gmail.com>
2026-04-13 01:47:37 +0930

    Add new algorithms and types for geodesic calculations; update .gitignore and result.json

---
+++ include/geodesic_lab/io/json_escape.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+
+static inline std::string jsonEscape(const std::string &s) {
+	std::string out;
+	out.reserve(s.size() + 8);
+	for (char c : s) {
+		switch (c) {
+		case '\\':
+			out += "\\\\";
+			break;
+		case '"':
+			out += "\\\"";
+			break;
+		case '\n':
+			out += "\\n";
+			break;
+		case '\r':
+			out += "\\r";
+			break;
+		case '\t':
+			out += "\\t";
+			break;
+		default:
+			out += c;
+			break;
+		}
+	}
+	return out;
+}
+
---
+++ include/geodesic_lab/types/vec3.hpp
@@ -0,0 +1,5 @@
+struct Vec3 {
+	double x;
+	double y;
+	double z;
+};
